63568854557316196d3eed5b4cc96a9f1bb7c523
F1ash69 <57477449+F1ash69@users.noreply.github.com>
2021-07-26 15:27:42 +0800

    Add files via upload

---
+++ address j1gger.py
@@ -0,0 +1,10 @@
+import random
+import string
+num = random.randint(1,9999999)
+alpha = random.choice(string.ascii_letters)
+b = random.choice(string.ascii_letters)
+c = random.choice(string.ascii_letters)
+d = random.choice(string.ascii_letters)
+e = random.choice(string.ascii_letters)
+f = random.choice(string.ascii_letters)
+print(str(alpha)+str(b)+str(c)+"type your address here"+str(d)+str(e)+str(f)) # you can add numbers as well
